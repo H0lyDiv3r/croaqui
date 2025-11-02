66833541340b4878303b6645cbfefc5cd66f118b
H0lyDiv3r <yohannesh.2018@gmail.com>
2025-11-02 11:52:46 +0300

    added react compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "18" }]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   build: {
     rollupOptions: {
       // external: ["@wails/go/player/Player"],
